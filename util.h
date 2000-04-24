16cd5cd3ab2d7985ac152868a7d8cb9ee362332f
dj <none@none>
2000-04-24 23:35:52 +0000

    Empty changelog

---
+++ util.h
@@ -0,0 +1,4 @@
+char*	hash2str( HV* hash );
+HV*	cdata2hash_Get( char *buf, int len, char *name, char *message );
+HV *	cdata2hash_Set( char *name, char *message );
+HV *	cdata2hash_Lookup( char *class, char *name, char *method );
